21c60b641e23e6976d59237f49d2e47d0168b604
fneumann <fneumann@databay.de>
2024-05-29 16:20:41 +0200

    Add MathJax document options

--- components/ILIAS/MathJax/resources/ilMathJaxBrowserConfig.js
+++ components/ILIAS/MathJax/resources/ilMathJaxBrowserConfig.js
@@ -1,10 +1,11 @@
-/**
- * Browser configuration for MathJax
- * Will be copied by composer install to public/assets/js/
- * @see \ILIAS\MathJax::init
- */
-
+// MathJax Configuration
+// see https://docs.mathjax.org/en/latest/options/index.html
 window.MathJax = {
+  options: {
+    // skipHtmlTags: {'[+]': ['body']}, // easy way to prevent MathJax except for allowed places
+    ignoreHtmlClass: 'tex2jax_ignore', //  class that marks tags not to search
+    processHtmlClass: 'tex2jax_process', //  class that marks tags that should be searched
+  },
   tex: {
     inlineMath: [
       ['[tex]', '[/tex]'],
